61d03310df166c9d4fdba0b820a09fbfb7641a06
Loc Nguyen <locnguyen@vklink.vn>
2022-09-15 16:21:37 +0700

    login screen

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/common/form_builder_password_field.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 
-import 'package:flutter_application_1/constants/keys.dart';
-import 'package:flutter_application_1/constants/route.dart';
 import 'package:flutter_application_1/store/models/root_store.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:provider/provider.dart';
 
@@ -17,7 +15,7 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormBuilderState>();
-  late final RootStore store;
+  RootStore? store;
 
   @override
   void initState() {
@@ -29,6 +27,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
     if (isFormValid) {
       print(_formKey.currentState!.value.toString());
+      store!.autoStore.login();
     } else {
       print("nhu hach");
     }
@@ -40,40 +39,59 @@ class _LoginScreenState extends State<LoginScreen> {
 
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(4.0),
-        child: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: FormBuilder(
+          key: _formKey,
+          autovalidateMode: AutovalidateMode.always,
+          onChanged: () {
+            _formKey.currentState!.saveAndValidate();
+          },
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              FormBuilder(
-                key: _formKey,
-                autovalidateMode: AutovalidateMode.always,
-                onChanged: () {
-                  _formKey.currentState!.saveAndValidate();
-                },
-                child: Column(
-                  children: [
-                    FormBuilderTextField(
-                      autovalidateMode: AutovalidateMode.always,
-                      name: 'userName',
-                      validator: FormBuilderValidators.required(),
-                      onChanged: (_) {
-                        _formKey.currentState!.saveAndValidate();
-                      },
-                    ),
-                    ElevatedButton(
-                      // onPressed: onLogin,
-                      onPressed: (_formKey.currentState?.validate() ?? false)
-                          ? onLogin
-                          : null,
-                      child: Observer(
-                        builder: (context) {
-                          return Text('Login token ${store.autoStore.token}');
-                        },
-                      ),
-                    )
-                  ],
-                ),
+              Text(
+                "Log In",
+                style: Theme.of(context).textTheme.headline1,
               ),
+              const SizedBox(
+                height: 20,
+              ),
+              FormBuilderTextField(
+                name: 'userName',
+                decoration: const InputDecoration(
+                  labelText: 'Username',
+                  border: OutlineInputBorder(),
+                ),
+                validator: FormBuilderValidators.required(),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              FormBuilderPasswordField(
+                name: 'password',
+                label: 'Password',
+                validator: FormBuilderValidators.required(),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: onLogin,
+                child: const Text("Login"),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Already have an account ?"),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text("Register !"),
+                  )
+                ],
+              )
             ],
           ),
         ),
